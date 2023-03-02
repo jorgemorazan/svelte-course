67e55768df55be3bd6b5f02cc0213b6239426b81
Rafael Morazan <rafaelmorazan09@gmail.com>
2023-03-02 16:45:21 -0600

    sass setup

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,13 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "src/styles/variables";`
+      }
+    }
+  },
   build: {
     rollupOptions: {
       input: "src/main.js"
