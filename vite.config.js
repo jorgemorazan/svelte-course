479ac14f66970046441d5a92ec57dad1703875a6
Rafael Morazan <rafaelmorazan09@gmail.com>
2023-03-02 15:23:03 -0600

    app component and parsed html in a variable

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: "src/main.js"
+    }
+  },
   plugins: [svelte()],
 })
