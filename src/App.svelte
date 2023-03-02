24cb84e01c1199d46955078ca80b8dec8337c2df
Rafael Morazan <rafaelmorazan09@gmail.com>
2023-03-02 16:00:12 -0600

    passing props

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,12 @@
 <script>
 	import Counter from './lib/Counter.svelte';
+
+	const props = {
+		initialCount: 3,
+		maxCount: 10
+	};
 </script>
 
-<Counter />
+<Counter {...props} />
 
 <style></style>
--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -1,13 +1,17 @@
 <script>
+	export let initialCount = 0;
+	export let maxCount = undefined;
 	let count = 0;
 	$: string = `You have clicked ${count}`;
 	$: if (count > 5) {
 		console.log(string);
 	}
-	const increment = () => {
+	function increment() {
+		if (count === maxCount) return;
 		count += 1;
-	};
+	}
 </script>
 
 <button on:click={increment}> CLICK ME! </button>
+<p>Initial count {initialCount}</p>
 <p>{string}</p>
