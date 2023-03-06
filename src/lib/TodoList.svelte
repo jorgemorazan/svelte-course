cd27e5777fc26e4d6b8164dae9d46ed698aa26af
Rafael Morazan <rafaelmorazan09@gmail.com>
2023-03-06 10:52:50 -0600

    updating an array state

--- src/lib/TodoList.svelte
+++ src/lib/TodoList.svelte
@@ -1,10 +1,33 @@
-<script>
+<script lang="ts">
+	import BaseButton from './BaseButton.svelte';
+	import { v4 as uuid } from 'uuid';
+
 	export let todos = [];
+	let inputText = '';
+
+	function handleAddTodo() {
+		if (!inputText) return;
+		// todos.push({
+		// 	id: uuid(),
+		// 	title: inputText,
+		// 	completed: false
+		// });
+		// todos = todos;
+		todos = [...todos, { id: uuid(), title: inputText, completed: false }];
+		inputText = '';
+	}
 </script>
 
-<ul>
-	{#each todos as { id, title }, index (id)}
-		{@const number = index + 1}
-		<li>{number} - {title}</li>
-	{/each}
-</ul>
+<div>
+	<ul>
+		{#each todos as { id, title }, index (id)}
+			{@const number = index + 1}
+			<li>{number} - {title}</li>
+		{/each}
+	</ul>
+	{inputText}
+	<form class="add-todo-form" on:submit|preventDefault={handleAddTodo}>
+		<input bind:value={inputText} />
+		<BaseButton type="submit" disabled={!inputText}>Add</BaseButton>
+	</form>
+</div>
