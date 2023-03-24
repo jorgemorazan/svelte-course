15a590d2d963f06b0d655b284ba8952afbe1b839
Rafael Morazan <rafaelmorazan09@gmail.com>
2023-03-23 21:25:08 -0600

    await blocks look fun

--- src/App.svelte
+++ src/App.svelte
@@ -5,29 +5,19 @@
 
 	let todoList;
 	let showList = true;
-	let todos = [
-		{
-			id: uuid(),
-			title: 'Todo 1',
-			completed: true
-		},
-		{
-			id: uuid(),
-			title: 'Todo 2',
-			completed: false
-		},
-		{
-			id: uuid(),
-			title: 'Todo 3',
-			completed: true
-		},
-		{
-			id: uuid(),
-			title:
-				'A long long long long long long long long long long long long long todo',
-			completed: true
-		}
-	];
+	let todos = null;
+	let promise = loadTodos();
+
+	function loadTodos() {
+		return fetch('https://jsonplaceholder.typicode.com/todos?_limit=10').then(
+			(response) => {
+				if (response.ok) {
+					return response.json();
+				}
+				throw new Error('An error has occurred');
+			}
+		);
+	}
 
 	async function handleAddTodo(event) {
 		event.preventDefault();
@@ -62,15 +52,26 @@
 	Show List
 </label>
 {#if showList}
-	<div style:max-width="400px">
-		<TodoList
-			{todos}
-			bind:this={todoList}
-			on:addtodo={handleAddTodo}
-			on:removetodo={handleRemoveTodo}
-			on:toggletodo={handleToggleTodo}
-		/>
-	</div>
+	{#await promise}
+		<p>Loading ...</p>
+	{:then todos}
+		<div style:max-width="400px">
+			<TodoList
+				{todos}
+				bind:this={todoList}
+				on:addtodo={handleAddTodo}
+				on:removetodo={handleRemoveTodo}
+				on:toggletodo={handleToggleTodo}
+			/>
+		</div>
+	{:catch error}
+		<p>{error.messages || 'An error has occured'}</p>
+	{/await}
+	<button
+		on:click={() => {
+			promise = loadTodos();
+		}}>Refresh</button
+	>
 {/if}
 
 <style></style>
