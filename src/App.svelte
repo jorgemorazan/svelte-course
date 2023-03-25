60d30d15460296b27883c82d6fe4b6eaab548a09
Rafael Morazan <rafaelmorazan09@gmail.com>
2023-03-25 14:27:08 -0600

    introduction to stores

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,33 @@
 <script>
-	import tippy from './lib/actions/tippy';
-	let content = 'hello';
+	import { onMount } from 'svelte';
+	import Home from './lib/pages/Home.svelte';
+	import Settings from './lib/pages/Settings.svelte';
+
+	let page;
+
+	function onRouteChange() {
+		const path = window.location.hash.slice(1);
+		if (path === '/') {
+			page = 'home';
+		} else if (path === '/settings') {
+			page = 'settings';
+		} else {
+			window.location.hash = '/';
+		}
+	}
+
+	onMount(onRouteChange);
 </script>
 
-<input bind:value={content} />
-<button use:tippy={{ content, placement: 'right', theme: 'light' }}
-	>Button</button
->
+<svelte:window on:hashchange={onRouteChange} />
+
+<nav>
+	<a href="#/">Home</a>
+	<a href="#/settings">Settings</a>
+</nav>
+
+{#if page === 'home'}
+	<Home />
+{:else if page === 'settings'}
+	<Settings />
+{/if}
---
+++ src/lib/pages/Settings.svelte
@@ -0,0 +1,36 @@
+<script>
+	import { onDestroy } from 'svelte';
+	import settings from '../stores/settings';
+	let colorScheme;
+	const unsubscribe = settings.subscribe(
+		(settings) => (colorScheme = settings.colorScheme)
+	);
+
+	$: {
+		settings.update((prevSettings) => ({ ...prevSettings, colorScheme }));
+	}
+
+	onDestroy(unsubscribe);
+</script>
+
+<h2>Settings</h2>
+
+<label>
+	<input
+		type="radio"
+		bind:group={colorScheme}
+		name="colorScheme"
+		value="dark"
+	/>
+	Dark
+</label>
+
+<label>
+	<input
+		type="radio"
+		bind:group={colorScheme}
+		name="colorScheme"
+		value="light"
+	/>
+	Light
+</label>
